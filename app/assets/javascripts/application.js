ed67c5720ff15fdb66813142d3bce10f2f8cdee1
Nick Oki <nick.oki@live.com>
2016-10-26 18:21:41 -0400

    Inject Angular Frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,126 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require angular
+//= require angular-resource
+//= require angular-rails-templates
+//= require_tree ../templates
+
+"use strict";
+
+// Note: function wrapped in an IFEE (Immediately Invoked Function Expression)
+(function() {
+
+  angular
+    .module("wdinstagram", [
+      "ui.router",
+      "ngResource",
+      "templates"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+    .factory("InstaFactory", [
+      "$resource",
+      InstaFactoryFunction
+    ])
+    .controller("InstaIndexController", [
+      "InstaFactory",
+      InstaIndexControllerFunction
+    ])
+    .controller("InstaNewController", [
+      "InstaFactory",
+      "$state",
+      InstaNewControllerFunction
+    ])
+    .controller("InstaShowController", [
+      "InstaFactory",
+      "$stateParams",
+      "$state",
+      InstaShowControllerFunction
+    ])
+    .controller("InstaEditController", [
+      "InstaFactory",
+      "$stateParams",
+      "$state",
+      InstaEditControllerFunction
+    ]);
+
+
+
+  // Factory Function
+  function InstaFactoryFunction($resource) {
+    return $resource("/entries/:id", {}, {
+      update: { method: "PUT" },
+      destroy: { method: "DELETE" }
+    })
+  }
+
+
+
+  // Controller Functions
+  function InstaIndexControllerFunction(InstaFactory) {
+    this.posts = InstaFactory.query()
+  }
+
+  function InstaNewControllerFunction(InstaFactory, $state) {
+    this.post = new InstaFactory()
+    this.create = function() {
+      this.post.$save().then(function(post) {
+        $state.go("instaShow", {id: post.id})
+      })
+    }
+  }
+
+  function InstaShowControllerFunction(InstaFactory, $stateParams, $state) {
+    this.post = InstaFactory.get({id: $stateParams.id})
+    // Delete function for posts
+    this.destroy = function() {
+      this.post.$delete({id: $stateParams.id}).then(function() {
+        $state.go("instaIndex")
+      })
+    }
+  }
+
+  function InstaEditControllerFunction(InstaFactory, $stateParams, $state) {
+    this.post = InstaFactory.get({id: $stateParams.id})
+    this.update = function() {
+      this.post.$update({id: $stateParams.id}).then(function(post) {
+        $state.go("instaShow", {id: post.id})
+      })
+    }
+  }
+
+
+
+  // Router
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("instaIndex", {
+      url: "/",
+      templateUrl: "ng-views/index.html",
+      controller: "InstaIndexController",
+      controllerAs: "vm"
+    })
+    .state("instaNew", {
+      url: "/new",
+      templateUrl: "ng-views/new.html",
+      controller: "InstaNewController",
+      controllerAs: "vm"
+    })
+    .state("instaShow", {
+      url: "/:id",
+      templateUrl: "ng-views/show.html",
+      controller: "InstaShowController",
+      controllerAs: "vm"
+    })
+    .state("instaEdit", {
+      url: "/:id/edit",
+      templateUrl: "ng-views/edit.html",
+      controller: "InstaEditController",
+      controllerAs: "vm"
+    })
+  };
+
+})(); // close IFEE
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :entries
+
+  root to: 'posts#index'
+
 end
